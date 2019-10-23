d52e746c8523673b4e78fd0fad1730a0b530f37e
Peter Moss <peter@moss.dk>
2019-10-23 08:01:14 +0200

    Added tests to controllers and fixed tests for Models

--- MusicGame.Controllers.Tests/UnitTest1.cs
+++ MusicGame.Controllers.Tests/UnitTest1.cs
@@ -1,16 +1,14 @@
 using System;
 using Xunit;
-using MusicGame.Models;
 
-namespace MusicGame.Models.Tests
+namespace MusicGame.Controllers.Tests
 {
     public class UnitTest1
     {
         [Fact]
         public void Test1()
         {
-            //var orchestra = new Orchestra();
-            //orchestra.Budget = 0;
+
         }
     }
 }
