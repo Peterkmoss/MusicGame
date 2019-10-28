747bdb582f3d26a634126f0073ab2954bfccf949
Mie Wetke Fust <wetkefust@gmail.com>
2019-10-28 20:51:51 +0100

    BuyPracticeRoom and tests

--- MusicGame.Models/Room.cs
+++ MusicGame.Models/Room.cs
@@ -5,5 +5,12 @@ namespace MusicGame.Models
         public int Size { get; private set; }
         public int Price { get; private set; }
         public string Location { get; private set; }
+
+        public Room(int size, int price, string location)
+        {
+            Size = size;
+            Price = price;
+            Location = location;
+        }
     }
 }
